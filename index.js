6941d5255912d4e065282e780d387868f864629e
wouter van der plas <wouterv.dplas@gmail.com>
2016-11-02 14:59:52 +0100

    some cleanup

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var deasync = require("deasync");
 var bot = require("./modules/mediawiki.js");
 var propertyFunc = require("./modules/esportsbot.js");
 
-var playerList = require("./modules/playerlist");
-
 var wtf = require("wtf_wikipedia");
 var nodemw = require("nodemw");
 
@@ -27,58 +25,4 @@ bot.login("esportsbot", "password", function (err, username) {
 
     console.log(bot.getClaims("q32"));
 
-    //for (i in playerList) {
-    //    var name = playerList[i];
-
-        //
-        //(function(uname) {
-
-        //    var done;
-        //
-        //    var wikitext = client.getArticle(name, function (err, info) {
-        //
-        //        if (err) {
-        //            console.log(err);
-        //        }
-        //
-        //        var wikiJson = wtf.parse(info);
-        //
-        //        var infoBox = wikiJson.infobox;
-        //
-        //        var props = Object.getOwnPropertyNames(infoBox);
-        //
-        //        if (typeof infoBox.id != "undefined" && typeof infoBox.id.text != "undefined"){
-        //            console.log("addding "+infoBox.id.text);
-        //        }
-        //
-        //        var itemId = bot.newItem(token);
-        //
-        //        var claimId;
-        //        claimId = bot.addClaim(token, itemId, "P2", bot.snakGenerator(bot.valueTypes.item, "Q40"));
-        //        bot.setReference(token, claimId, bot.referencesGen(bot.valueTypes.item, "P89", "Q14"));
-        //
-        //        claimId = bot.addClaim(token, itemId, "P77", bot.snakGenerator(bot.valueTypes.string, uname));
-        //        bot.setReference(token, claimId, bot.referencesGen(bot.valueTypes.item, "P89", "Q14"));
-        //
-        //        claimId = bot.addClaim(token, itemId, "P92", bot.snakGenerator(bot.valueTypes.item, "Q36"));
-        //        bot.setReference(token, claimId, bot.referencesGen(bot.valueTypes.item, "P89", "Q14"));
-        //
-        //
-        //        for (p in props) {
-        //            var func = propertyFunc[props[p]];
-        //
-        //            if (typeof func != "undefined") {
-        //                func(infoBox[props[p]], itemId, token);
-        //            }
-        //
-        //            console.log(p+"/"+props.length);
-        //        }
-        //        done = true;
-        //    });
-        //
-        //    while (done === undefined) {
-        //        deasync.runLoopOnce();
-        //    }
-        //}(name))
-    //}
 });
